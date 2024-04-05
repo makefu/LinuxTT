607106f1dd249a5f317480d378ad9a3b6a0cb4e7
makefu <github@syntax-fehler.de>
2024-04-05 19:11:21 +0200

    config: init folder

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
         system = "x86_64-linux";
         modules = [
           "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
-          ./customizations.nix
+          ./config/customizations.nix
         ];
       };
     };
