bcbf4e57af0de9baffa0bc7b6028f67eb1354b8a
makefu <github@syntax-fehler.de>
2024-04-06 13:45:00 +0200

    flake.nix: add disko config

--- flake.nix
+++ flake.nix
@@ -1,8 +1,12 @@
 {
   description = "Minimal NixOS installation media";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.disko = {
+    url = "github:nix-community/disko";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
 
-  outputs = { self, nixpkgs }@inputs: let
+  outputs = { self, nixpkgs,disko }@inputs: let
     system = "x86_64-linux"; # linuxTT is currently only available for x86_linux
     pkgs = (import nixpkgs {
       inherit system;
@@ -18,6 +22,7 @@
     packages."${system}" = {
       isoImage = self.nixosConfigurations.isoImage.config.system.build.isoImage;
       default = self.packages."${system}".isoImage;
+      diskImage = selfnixosConfigurations.diskImage.config.system.build.diskoImagesScript
     };
 
     nixosConfigurations = {
@@ -25,9 +30,18 @@
         inherit pkgs system;
 
         modules = [
+          disko.nixosModules.disko
           #"${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-graphical-calamares-gnome.nix"
           "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-graphical-plasma5.nix" 
-          ./config/customizations.nix
+          ./config/iso-customizations.nix
+        ];
+      };
+      diskImage = nixpkgs.lib.nixosSystem {
+        inherit pkgs system;
+
+        modules = [
+          ./config/disk/hybrid.nix
+          ./config/disk-customizations.nix
         ];
       };
     };
