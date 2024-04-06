cb754855bfe535bca74c2041a14cdda46f5fba56
makefu <github@syntax-fehler.de>
2024-04-06 03:41:20 +0200

    tools: add a minimal set pentesting tools

--- config/core.nix
+++ config/core.nix
@@ -1,3 +1,14 @@
+{ pkgs, ... }:
 {
   nix.settings.experimental-features = [ "flakes" "nix-command" ];
+  environment.systemPackages = with pkgs; [
+    p7zip
+    parted
+    xrdp
+    tmux
+    curl
+    wget
+    htop
+    # tightvnc
+  ];
 }
