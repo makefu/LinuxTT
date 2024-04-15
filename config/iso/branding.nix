bfcf8ccbc225f9031855d9ed15e10ef18e0754c6
makefu <github@syntax-fehler.de>
2024-04-16 00:30:14 +0200

    branding: use custom background for grub

--- config/iso/branding.nix
+++ config/iso/branding.nix
@@ -4,9 +4,7 @@
   isoImage.volumeID = "ttlinux-${config.system.nixos.release}-${pkgs.stdenv.hostPlatform.uname.processor}";
   isoImage.appendToMenuLabel = " LiveCD";
 
-  isoImage.splashImage = pkgs.fetchurl {
-    url = "https://raw.githubusercontent.com/NixOS/nixos-artwork/a9e05d7deb38a8e005a2b52575a3f59a63a4dba0/bootloader/isolinux/bios-boot.png";
-    sha256 = "1wp822zrhbg4fgfbwkr7cbkr4labx477209agzc0hr6k62fr6rxd";
-  };
-
+  isoImage.splashImage = ../../assets/grub/1.png;
+  # isoImage.efiSplashImage = ../../assets;
+  # isoImage.grubTheme
 }
